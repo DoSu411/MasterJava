630b13e46994972335389abecb10292e630773ec
Dominik Summa <DoSu411@web.de>
2015-08-17 17:24:22 +0200

    First comit of package lession one with the class MasterOne

---
+++ MasterJava/src/lession/one/MasterOne.java
@@ -0,0 +1,12 @@
+package lession.one;
+
+public class MasterOne {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// Test this
+		System.out.println("Hello, i am your master!");
+	}
+
+}
